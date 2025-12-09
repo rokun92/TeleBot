3d285e0105fa297abaed279db7823fd05b221e8f
rokun92 <2022331092@student.sust.edu>
2025-12-10 02:17:31 +0600

    No No

---
+++ src/main/java/botcode/Bot.java
@@ -0,0 +1,47 @@
+package botcode;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "demon_dice_bot";
+    }
+
+    Dotenv dotenv = Dotenv.load();
+
+    @Override
+    public String getBotToken() {
+        return dotenv.get("BOT_TOKEN");
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String chatId = update.getMessage().getChatId().toString();
+            String userText = update.getMessage().getText();
+
+            SendMessage message = new SendMessage();
+            message.setChatId(chatId);
+            message.setText("Fuck You");
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        Bot bot = new Bot();
+        botsApi.registerBot(bot);
+    }
+}
